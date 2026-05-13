a28f66a747dbb93d0a86ae4b2b588aa99d942886
yazed_hassan <y.zreak@gmail.com>
2026-05-13 18:23:19 +0300

    Refactor brightness minimum settings and add CI build workflow

--- app/src/main/kotlin/com/yhpgi/brightnessfix/BrightnessFix.kt
+++ app/src/main/kotlin/com/yhpgi/brightnessfix/BrightnessFix.kt
@@ -6,11 +6,22 @@ import de.robv.android.xposed.IXposedHookZygoteInit
 class BrightnessFix : IXposedHookZygoteInit {
 
     override fun initZygote(startupParam: IXposedHookZygoteInit.StartupParam?) {
+        val minBrightnessInt = 1
+        val minBrightnessFloat = minBrightnessInt / 255f
+
         XResources.setSystemWideReplacement(
             "android",
             "integer",
             "config_screenBrightnessSettingMinimum",
-            1
+            minBrightnessInt
+        )
+
+        // Android 16 QPR2 uses the float config for clamping the slider minimum.
+        XResources.setSystemWideReplacement(
+            "android",
+            "dimen",
+            "config_screenBrightnessSettingMinimumFloat",
+            minBrightnessFloat
         )
     }
 
